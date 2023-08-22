c066e5ad6267ed4ebde8d6b68d52e28d9486ea84
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-08-22 10:55:36 +0300

    Using viper to read env variables

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,26 +7,20 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-
+	"github.com/techschool/simplebank/util"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	driverName = "postgres"
-)
-
 func TestMain(m *testing.M)  {
 	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
 
-	// Read the POSTGRES_PASSWORD environment variable
-    	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
-
-    	// Construct the dataSourceName
-   	dataSourceName := "postgresql://root:" + postgresPassword + "@localhost:5432/simple_bank?sslmode=disable"
-
-	testDB, err = sql.Open(driverName, dataSourceName)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
--- main.go
+++ main.go
@@ -4,19 +4,20 @@ import (
 	"database/sql"
 	"log"
 
+	_ "github.com/lib/pq"
 	"github.com/techschool/simplebank/api"
 	db "github.com/techschool/simplebank/db/sqlc"
-	_ "github.com/lib/pq"
-)
-
-const (
-	driverName = "postgres"
-	dataSourceName = "postgresql://root:roba@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/techschool/simplebank/util"
 )
 
 func main() {
-	conn, err := sql.Open(driverName, dataSourceName)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -24,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewSever(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
